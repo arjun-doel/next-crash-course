97565c111e36cb5ee0295615f0948dc46af2b303
Arjun Doel <arjundoel@gmail.com>
2021-08-18 17:21:10 +0100

    article api fetch

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
         <title>WebDev Newz</title>
         <meta name='keywords' content='web dev, programming'/>
       </Head>
-      <h1>Welcome to Next</h1>
+      {articles.map(ite => <h3 key={ite.id}>{ite.title}</h3>)}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
